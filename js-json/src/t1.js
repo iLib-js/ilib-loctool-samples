e6cb4f49ac6db01f635bc890ecfed35e9d65a93c
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2022-04-26 08:19:04 -0700

    Sample with js input and json output

---
+++ js-json/src/t1.js
@@ -0,0 +1,15 @@
+var fs = require("foobar");
+
+var t1 = function() {
+    console.log("If you find this, you found the wrong string!");
+}
+
+t1.prototype.tester = function() {
+    rb.getString("This is a test");
+    rb.getString("This is a test with a unique id", "id1");
+    rb.getStringJS("Hello");
+    rb.
+        getString(
+            "Thank you"
+        );
+};
