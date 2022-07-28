8960bd2876d99b4b387606b65e088e8b23d05984
Goun Lee <goun.lee@lge.com>
2022-07-28 17:58:41 +0900

    temp update for testing

--- webos-js/src/msg.js
+++ webos-js/src/msg.js
@@ -1,6 +1,6 @@
 import $L from '@enact/i18n/$L';
 export function findMsgByCode (code) {
-    switch (code) {
+    /*switch (code) {
         case 1:
             msg.reason = $L('Hello'); // i18n : i18n comments
             break;
@@ -17,7 +17,7 @@ export function findMsgByCode (code) {
             msg.reason = $L('Bye');
             break;
     }
-    return msg;
+    return msg;*/
 }
 export function findMsgByCode2 (code) {
     switch (code) {
