c80338c71463a1aa182d1aafce574c90a681117b
Goun Lee <goun.lee@lge.com>
2024-01-05 16:43:01 +0900

    Update webos-dart sample

--- webos-dart/src/msg.dart
+++ webos-dart/src/msg.dart
@@ -1,7 +1,7 @@
-translate("Search");
+translate("Search", key: "Search_all");
 translate("Back button");
 translate("Delete All");
-translate("App List", key: "music_track");
+translate("App List");
 translate("App Rating", args: {"arg1": "music_track"});
 
 translatePlural('1#At least 1 letter|#At least {num} letters', num);
