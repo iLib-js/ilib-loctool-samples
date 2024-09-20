1c7bbda95d61619d4110637e0013ef302ca48ac2
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2024-09-19 18:15:01 -0700

    Added comments to the source file as an example

--- slack/translations/strings.js
+++ slack/translations/strings.js
@@ -1,6 +1,13 @@
 export default messages = {
+    // comment for string1
     "string1": "Hello",
+
+    // comment for string2
     "string2": "Thank *you*",
+
+    // another comment, this time for string3
     "string3": "This _is a *test* with a unique id_",
+
+    // string4 is the target of this comment
     "string4": "This is a *test* with a slack <http://www.slack.com/|link> in it and a <@user|User> reference."
 };
